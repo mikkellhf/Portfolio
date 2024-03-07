e342cb53e62137d933d544475c94dc3ba30f45e1
mikkellhf <prd882@alumni.ku.dk>
2024-03-07 14:05:34 +0100

    Update game_loop.py

--- Terminal_Games/game_loop.py
+++ Terminal_Games/game_loop.py
@@ -1,3 +1,29 @@
 
+# Have a functio that clears the terminal
+# Have a class of functions that draws the game board (depending on the game)
+    # These functions should call the clear terminal function
 
-def 
+
+def clear_terminal():
+    print("\033c", end="")
+    return
+
+def draw_game_board():
+    clear_terminal()
+    print("Drawing the game board...")
+    print("This is the game board")
+    print("-----------------------")
+    print("|     |     |     |")
+    print("-----------------------")
+    pass
+
+def main():
+    print("HEY")
+    while (1):
+        if (input() == "q"):
+            break
+        else :
+            draw_game_board()
+    pass
+
+main()
