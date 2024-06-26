596ddf9296dbb1219d3070f412752c1d42ab1ba0
Mikkellhf <prd882@alumni.ku.dk>
2024-06-26 09:12:12 +0200

    Updated css/js

--- html/js/terminal.js
+++ html/js/terminal.js
@@ -1,1 +1,8 @@
+// terminal.js
 
+document.addEventListener('DOMContentLoaded', (event) => {
+    var term = new Terminal();
+    term.open(document.getElementById('terminal'));
+    term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ');
+  });
+  
