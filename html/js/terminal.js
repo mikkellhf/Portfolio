f00cbe4adbdbfee63938fdc0cf721196ffbe1691
Mikkellhf <prd882@alumni.ku.dk>
2024-06-28 16:52:41 +0200

    Update terminal.js

--- html/js/terminal.js
+++ html/js/terminal.js
@@ -4,5 +4,19 @@ document.addEventListener('DOMContentLoaded', (event) => {
     var term = new Terminal();
     term.open(document.getElementById('terminal'));
     term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ');
+    let myBuffer = [];
+
+    term.on('key', function(key, e) {
+        myBuffer.push(key);
+    });
+
+    term.on('lineFeed', function() {
+        let keysEntered = myBuffer.join('');  // Or something like that
+        
+        myBuffer = [];  // Empty buffer
+    });
   });
+
+
+  
   
