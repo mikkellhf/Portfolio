ff2fa01b787894589d77fd8d8d9894ad2475810c
mikkellykkefrandsen <mikkellykkefrandsen@gmail.com>
2024-10-10 16:29:28 +0200

    Added flowfields and fractals

--- Projects/flowfields/efficient_flowfield.js
+++ Projects/flowfields/efficient_flowfield.js
@@ -1,10 +1,22 @@
+//Setup
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
+const root = document.documentElement;
+const scale = 10;
 
-float scale = 10:
-int rows,cols;
 
-void setup() {
-    canvas.width = 800;
-    canvas.height = 800;
+
+// Canvas settings
+ctx.fillStyle = 'white';
+ctx.strokeStyle = 'white';
+ctx.lineWidth = 1;
+canvas.width = 500;
+canvas.height = 500;
+function setup() {
+    rows = new int(canvas.width/scale)
+    cols = new int(canvas.width/scale)
+
 }
+
+
+
